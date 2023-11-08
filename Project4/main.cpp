155a62b580c09c584bf7780a3fd4478475502002
ThDat1016 <125478305+ThDat1016@users.noreply.github.com>
2023-11-08 20:51:52 +0700

    Update main.cpp

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,67 +1,15 @@
-﻿#include "khaibao.h"
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include "khaibao.h"
 
 int main() {
-    ListSach list_sach;
-    int choice;
-    cin.ignore(); // Loại bỏ dấu xuống dòng sau khi đọc lựa chọn.
+    ListSach listSach;
+    ListSV listSV;
+    int choice = 1;
+    int maxChoice = 10; // Tổng số lựa chọn trong menu (đã sửa thành 10)
+    static string tenSach, tacGia, mssv;
+    static int namXuatBan, maSach;
 
     while (true) {
-        cout << "\nMENU:";
-        cout << "\n1. Them Sach";
-        cout << "\n2. Danh Sach Cac Cuon Sach(TQ)";
-        cout << "\n3. Danh Sach Cac Cuon Sach(CT)";
-        cout << "\n4. Tim Kiem Theo Ten";
-        cout << "\n5. Ghi File";
-        cout << "\n6. Thoat Chuong Trinh";
-        cout << "\nNhap lua chon: ";
-        cin >> choice;
-        cin.ignore(); // Loại bỏ dấu xuống dòng sau khi đọc lựa chọn.
-
-        string tenSach, tacGia;
-        int namXuatBan;
-        pressAnyKey();
-        switch (choice) {
-        case 1: {
-            cout << "Nhap thong tin sach:" << endl;
-
-            cout << "Ten sach: ";
-            getline(cin, tenSach); // Đọc tên sách từ dòng nhập.
-
-            cout << "Tac gia: ";
-            getline(cin, tacGia); // Đọc tên tác giả từ dòng nhập.
-
-            cout << "Nam xuat ban: ";
-            cin >> namXuatBan; // Đọc năm xuất bản.
-
-            list_sach.addSach(tenSach, tacGia, namXuatBan);
-            cout << "Sach da duoc them vao danh sach." << endl;
-            pressAnyKey();
-            break;
-        }
-        case 2:
-            // In ra thông tin sách đã nhập
-            list_sach.PrintSachTQ();
-            break;
-        case 3:
-            list_sach.PrintSach();
-            break;
-        case 4: {
-            cout << "Nhap Ten Sach: ";
-            getline(cin, tenSach);
-            list_sach.searchBook(tenSach);
-            break;
-        }
-        case 5:
-            list_sach.saveToFile("ThongTin.xls");
-            cout << "Danh sach da duoc ghi vao file ThongTin.txt." << endl;
-            break;
-        case 6:
-            return 0;
-        default:
-            cout << "Lua chon khong hop le. Hay nhap lai." << endl;
-        }
+        if (menu(listSach, listSV, tenSach, tacGia, mssv, namXuatBan, maSach, choice, maxChoice) == 0) return 0;
     }
 
     return 0;
