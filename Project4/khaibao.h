cb34492b1ff86ff9a285d270619e904860259210
ThDat1016 <125478305+ThDat1016@users.noreply.github.com>
2023-11-08 20:50:50 +0700

    Update khaibao.h

--- Project4/khaibao.h
+++ Project4/khaibao.h
@@ -1,5 +1,4 @@
-﻿#pragma once
-
+#pragma once
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -8,21 +7,30 @@
 #include <functional>
 #include<random>
 #include<set>
+#include<Windows.h>
+#include<conio.h>
+// Định nghĩa màu sắc cho chữ (đỏ)
+#define RED_TEXT SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
+
+// Đặt lại màu sắc chữ về mặc định
+#define RESET_TEXT SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
 using namespace std;
 
 
 class Sach {
 public:
-	string TenSach, TacGia;
-	int NamXuatBan, SoLuong;
-	int ID, temp;
+	string TenSach, TacGia, TinhTrang, SinhVienMuon;
+	int NamXuatBan, SoLuong, ID;
+	Sach() {};
 	Sach(const string& TenSach, const string& TacGia, int NamXuatBan) {
 		this->NamXuatBan = NamXuatBan;
-		this->SoLuong = 0;
+		SoLuong = 0;
 		this->TacGia = TacGia;
 		this->TenSach = TenSach;
-		this->ID = randomId();
-		this->temp = 0;// bien danh dau xem sach da duoc cho muon hay chua
+		ID = randomId();
+		SinhVienMuon = "";// neu co sv muon thi gan mssv vao bien nay, tra sach thi quay ve = 0
+		TinhTrang = "0";
 	}
 	int randomId() const {
 		random_device rd;
@@ -52,63 +60,93 @@ public:
 
 	void PrintSachTQ();
 	void PrintSach();
-	//void sort_AZ(); // sap xep chu cai
 	void searchBook(const string& TenSach);
 	void GetSachID(const string& TenSach, const string& TacGia, int NamXuatBan);
-	//void deleteBook();
+	void deleteBook(const string& id);
 	void saveToFile(const string& filename);
-	long SoLuong();
+	void SoLuong();
+
 };
 
-//class NodeSv; // Forward declaration of NodeSv
-//
-//class SV {
-//private:
-//    string MSSV;
-//	string TenSv, Lop, sachMuon, tacgia;
-//	int NXB, idSach;
-//public:
-//    
-//    SV(const string& TenSv, const string& Lop, const string& MSSV, const string& sachMuon, const string& tacgia, int NXB, int idSach) {
-//        this->Lop = Lop;
-//        this->MSSV = MSSV;
-//        this->TenSv = TenSv;
-//		this->sachMuon = sachMuon;
-//		this->tacgia = tacgia;
-//		this->NXB = NXB;
-//		this->idSach = idSach;
-//    }
-//    string GetMSSV() const { // Changed return type to string
-//        return MSSV;
-//    }
-//    void SetMSSV(const string& newMSSV) { // Changed parameter type to const string&
-//        MSSV = newMSSV;
-//    }
-//};
-//
-//class NodeSv {
-//public:
-//    SV ThongTinSV;
-//    NodeSv* nextSv;
-//
-//    NodeSv(SV& ThongTinSV) : ThongTinSV(ThongTinSV), nextSv(nullptr) {}
-//};
-//
-//class ListSV {
-//private:
-//    NodeSv* headSv = nullptr;
-//    NodeSv* tailSv = nullptr;
-//
-//public:
-//	Node* GetHeadSv() {
-//		return headSv;
-//	}
-//	void SetHeadSv(NodeSv* head) {
-//		headSv = head;
-//	}
-//	void addSv(const string& TenSach, const string& TacGia, int NamXuatBan, const string& tenSV, const string& lop, const string& mssv);
-//	void TraSach(const string& mssv, const string& TrinhTrang);
-//};
+class NodeSv;
+class SV {
+private:
+	string MSSV;
+	string TenSv, Lop;
+public:
+	SV(const string& TenSv, const string& Lop, const string& MSSV) {
+		this->Lop = Lop;
+		this->MSSV = MSSV;
+		this->TenSv = TenSv;
+	}
+	string GetMSSV() const {
+		return MSSV;
+	}
+	void SetMSSV(const string& newMSSV) { 
+		MSSV = newMSSV;
+	}
+	string GetTenSv() const {
+		return TenSv;
+	}
+	void SetTenSv(const string& ten) {
+		TenSv = ten;
+	}
+	string GetLop() const {
+		return Lop;
+	}
+	void SetLop(const string& lop){
+		Lop = lop;
+	}
+};
+struct TTSach {
+	string ten,tacgia;
+	int nxb, id;
+};
+
+class NodeSv {
+public:
+    SV ThongTinSV;
+	TTSach* thong_tin_sm;
+	int size, sizeUse;
+    NodeSv* next;
+	NodeSv(SV& ThongTinSV) : ThongTinSV(ThongTinSV), thong_tin_sm(nullptr), next(nullptr), size(20), sizeUse(0) { thong_tin_sm = new TTSach[20]; }
+	~NodeSv() { delete []thong_tin_sm; }
+};
+
+class ListSV {
+private:
+	NodeSv* father;
+	NodeSv* tailSv;
+public:
+	ListSV() { father = tailSv = nullptr; }
+	NodeSv* GetFather() {
+		return father;
+	}
+	void SetFaTher(NodeSv* head) {
+		father = head;
+	}
+	void addSv(const string& tenSV, const string& lop, const string& mssv);
+	void DeleteSV(const string& mssv);
+	void ShowSV();
+	void swapSV(NodeSv* a, NodeSv* b);
+	NodeSv* partition(NodeSv* low, NodeSv* high);
+	void quickSort(NodeSv* low, NodeSv* high);
+	void saveSvToFile(const string& fileName);
+	void SortByName();
+	NodeSv* getTail(NodeSv* current);
+	void addSM(NodeSv* sv, ListSach& bookList, const string& ten, const string& tacgia, int nxb);
+	void Muon_Sach(const string& mssv, ListSach& listSach, ListSV& list);
+	void TraSach(ListSV& listSv, ListSach& bookList, const string& mssv, int maSach);
+};
 
 void pressAnyKey();
 void saveToFile(const string & fileName); 
+void printRedText(const std::string& text);
+void highlightOption(int currentOption, int totalOptions);
+void displayMenuAdmin(int currentOption, int totalOptions);
+//void addSM(const string& ten, const string& tacgia, int nxb, const string& tenSv, const string& lop, const string& mssv);
+//void Muon_Sach(const string& mssv, ListSach& listSach, ListSV& list);
+//void TraSach(ListSV& listSv,ListSach& bookList, const string& mssv, int maSach);
+int menu(ListSach& listSach, ListSV& listSV, string& tenSach, string& tacGia, string& mssv, int namXb, int maSach, int& choice, int& maxChoice);
+void GoTo(SHORT posX, SHORT posY);
+void DanhSachSachMuon(NodeSv* sv);
